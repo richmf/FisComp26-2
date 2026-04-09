380da5f254eb7a3566f3c23a0eae7eebcc8cea95
richmf <rich@ciencias.unam.mx>
2026-04-09 10:00:38 -0600

    ok

--- mi_primer_cortometraje.py
+++ mi_primer_cortometraje.py
@@ -5,13 +5,23 @@ from matplotlib.animation import FuncAnimation
 fig , ejes = plt.subplots()
 x = np.linspace(0,2*np.pi)
 
-linea, = ejes.plot(x,np.cos(x))
+#linea, = ejes.plot(x,np.cos(x))
+linea, = ejes.plot([],[])
+ejes.set_xlim(x[0],x[-1]) , ejes.set_ylim(-1.1,1.1)
 
+
+def inicio():
+    linea.set_data([],[])
+    ejes.set_xlim(x[0],x[-1]) , ejes.set_ylim(-1.1,1.1)
+    return linea,
+    
 def cuadro(n):
-    linea.set_ydata( np.cos(x - n/20) )
+    linea.set_data( x , np.cos(x - n/20) )
     return linea,
 
-animacion = FuncAnimation(fig,cuadro,frames=100, interval = 45,blit=False)
+animacion = FuncAnimation(fig , cuadro , frames=100 , interval = 45 ,
+                          init_func=inicio , blit=True)
+#animacion = FuncAnimation(fig , cuadro , frames=100 , interval = 45 , blit=True)
 
 #animacion.save('mi_peli.mp4')
 
